4bd4a2cc47e7c29af1539801269ffa8b852cd45a
Arman-PC <Arman-PC@ARMAN-DESKTOP>
2023-01-25 09:47:00 +0330

    format codes

--- application/src/main/java/com/tosan/application/Application.java
+++ application/src/main/java/com/tosan/application/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"com.tosan.*"})
 @EnableJpaRepositories(basePackages = {"com.tosan.repository"})
-@EntityScan(basePackages ="com.tosan.model")
+@EntityScan(basePackages ={"com.tosan.model"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
