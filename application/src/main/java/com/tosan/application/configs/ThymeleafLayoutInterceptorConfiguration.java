c1fd24c4d5dd187d717942782db1a5f7945a0306
arman-arian <arman.hassanpoor2000@gmail.com>
2023-02-09 17:41:49 +0330

    optimise imports

--- application/src/main/java/com/tosan/application/configs/ThymeleafLayoutInterceptorConfiguration.java
+++ application/src/main/java/com/tosan/application/configs/ThymeleafLayoutInterceptorConfiguration.java
@@ -2,10 +2,11 @@ package com.tosan.application.configs;
 
 import com.tosan.application.extensions.thymeleaf.ThymeleafLayoutInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ThymeleafLayoutInterceptorConfiguration implements WebMvcConfigurer  {
+public class ThymeleafLayoutInterceptorConfiguration implements WebMvcConfigurer {
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new ThymeleafLayoutInterceptor());
     }
