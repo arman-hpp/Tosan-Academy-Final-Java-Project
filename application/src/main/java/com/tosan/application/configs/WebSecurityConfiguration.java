252ad294192c455190093a9b6e795f8fc1254974
arman-arian <arman.hassanpoor2000@gmail.com>
2023-02-11 10:31:13 +0330

    add spring boot security configs

---
+++ application/src/main/java/com/tosan/application/configs/WebSecurityConfiguration.java
@@ -0,0 +1,88 @@
+package com.tosan.application.configs;
+
+import com.tosan.application.configs.handlers.CustomAccessDeniedHandler;
+import com.tosan.application.configs.handlers.CustomAuthenticationFailureHandler;
+import com.tosan.application.configs.handlers.CustomAuthenticationSuccessHandler;
+import com.tosan.application.configs.handlers.CustomLogoutSuccessHandler;
+import com.tosan.core_banking.services.AuthenticationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfiguration {
+    public final AuthenticationService _authenticationService;
+
+    public WebSecurityConfiguration(AuthenticationService authenticationService) {
+        _authenticationService = authenticationService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                                .requestMatchers("/h2/**").permitAll()
+                                .requestMatchers("/auth/**", "/register/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/", "/index").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/static/**").permitAll()
+                                .requestMatchers(HttpMethod.GET,  "/errors/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/auth/login")
+                        .successHandler(authenticationSuccessHandler())
+                        .failureHandler(authenticationFailureHandler())
+                        .permitAll()
+                )
+                .logout((form) -> form
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login")
+                        .logoutSuccessHandler(logoutSuccessHandler())
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                )
+                .userDetailsService(_authenticationService)
+                .httpBasic().disable()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler(_authenticationService);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler(_authenticationService);
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler();
+    }
+}
